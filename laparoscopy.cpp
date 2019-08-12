4d69f3c68ec5058ea3cd32dab4395e2a0d85a658
sanine-a <sanine.not@pm.me>
2019-08-12 16:45:18 -0500

    add moving text on screensaver

--- laparoscopy.cpp
+++ laparoscopy.cpp
@@ -4,6 +4,7 @@
 #include <iostream>
 #include <string>
 #include <chrono>
+#include <cstdlib>
 
 using namespace std::chrono;
 using namespace std;
@@ -11,12 +12,19 @@ using namespace cv;
 
 milliseconds diff_check_interval(10000); // check for difference every 10 seconds
 
+milliseconds text_move_interval(10000); // move on-screen text every 10 seconds
+
 string window_name = "Laparascopy";
 string difference_fname = "diffcheck.png";
+string wait_text = "Push button to start.";
 
-double difference_threshold = 0.03; // images less than 10% different are deemed the same
+double difference_threshold = 0.03; // images less than 3% different are deemed the same
 
+// function to check the level of difference between two images
 double get_difference( Mat, Mat );
+
+// function to build a frame of the screensaver
+Mat make_waitscreen( int, int, string );
 
 int main( int argc, char** argv ) {
   // set up wiring pi
@@ -34,6 +42,7 @@ int main( int argc, char** argv ) {
   int screen_h = screen->height;
   cout << "Screen size: " << screen_w << "x" << screen_h << endl;
 
+  // open camera
   VideoCapture camera(0);
   if ( !camera.isOpened() ){
     cerr << "ERROR: could not open camera!" << endl;
@@ -42,27 +51,16 @@ int main( int argc, char** argv ) {
   camera.set(CAP_PROP_BUFFERSIZE, 1); // buffer only one frame
   cout << "opened camera" << endl;
 
+  // create main window
   namedWindow(window_name, WINDOW_NORMAL | WINDOW_FULLSCREEN );
-  //  setWindowProperty( window_name, WND_PROP_FULLSCREEN, WINDOW_FULLSCREEN );
 
+  // mats for pulling from the camera and displaying
   Mat frame, oldframe, display;
   camera >> oldframe;
 
-  
-  
-  string text = "Push button to start.";
-  int baseline = 0;
-  Size textsize = getTextSize( text, FONT_HERSHEY_DUPLEX, 1, 1, &baseline );
-
-  Mat waitscreen ( screen_h, screen_w, CV_8UC3, Scalar(64,64,64) );
-  putText( waitscreen, text,
-	   Point( (waitscreen.cols-textsize.width)/2, (waitscreen.rows-textsize.height)/2 ),
-	   FONT_HERSHEY_SIMPLEX, 1, Scalar(255,255,255), 1, LINE_AA );
-
-  cout << "waitscreen size: " << waitscreen.rows << "x" << waitscreen.cols << endl;
+  display = make_waitscreen();
   
   bool showcamera = false;
-  display = waitscreen.clone();
 
   auto timepoint = steady_clock::now();
 
@@ -70,39 +68,57 @@ int main( int argc, char** argv ) {
   
   while (true) {
     if (showcamera) {
-      camera >> frame;
+      camera >> frame; // get frame from the camera
+
+      // has the difference time been exceded?
+      if (steady_clock::now() >= timepoint + diff_check_interval ) {
+	// time since last timepoint is greater than diff_check_interval
+	if ( get_difference( frame, oldframe ) >= difference_threshold ) {
+	  // someone's still using the exhibit!
+	  oldframe = frame.clone();
+	  timepoint = steady_clock::now();
+	}
+	else {
+	  // nothing's changed since last we checked
+	  // turn off the camera
+	  showcamera = false;
+	  camera >> frame;
+	  display = make_waitscreen( screen_w, screen_h, wait_text );
+	  digitalWrite(led_pin,1); // turn on led
+	  timepoint = steady_clock::now();
+	}
+      }
+      // resize frame for display.
+      // we need to do the resizing manually, because we need to force
+      // nearest-neighbor interpolation, as the default linear interpolation
+      // is too intensive for the Pi to keep up with and produces noticeable
+      // lag.
       resize( frame, display, Size(screen_w, screen_h), 0, 0, INTER_NEAREST );
     }
     else {
+      // showcamera is false
+      // check if the button has been pressed
       if ( digitalRead(button_pin) ) {
+	// it has!
         digitalWrite(led_pin,0); // turn off led
 	showcamera = true;
+	// get a frame for difference checking
 	camera >> frame;
 	oldframe = frame.clone();
+	
+	timepoint = steady_clock::now();
+      }
+
+      // check if the time has exceeded text_move_interval
+      if (steady_clock::now() >= timepoint + text_move_interval ) {
+	display = make_waitscreen( screen_w, screen_h, wait_text );
 	timepoint = steady_clock::now();
       }
     }
 
+    // display whatever we're looking at
     imshow( window_name, display );
 
-    if ( showcamera &&
-	 (steady_clock::now() >= timepoint + diff_check_interval ) ) {
-      // time since last timepoint is greater than diff_check_interval
-      if ( get_difference( frame, oldframe ) >= difference_threshold ) {
-	// someone's still using the exhibit!
-	oldframe = frame.clone();
-	timepoint = steady_clock::now();
-      }
-      else {
-	// nothing's changed since last we checked
-	// turn off the camera
-	showcamera = false;
-	camera >> frame;
-	display = waitscreen.clone();
-	digitalWrite(led_pin,1); // turn on led
-      }
-    }
-    
     switch (waitKey(1)) {
     case 27:
       cout << "quitting!" << endl;
@@ -136,4 +152,20 @@ double get_difference( Mat src1, Mat src2 ) {
   int n_different = countNonZero(diff);
 
   return (double) n_different / (gray1.rows * gray1.cols); // fraction different
+}
+
+
+Mat make_waitscreen( int screen_w, int screen_h, string text ) {
+  int baseline = 0;
+  Size textsize = getTextSize( text, FONT_HERSHEY_DUPLEX, 1, 1, &baseline );
+
+  int x = rand() % ( screen_w - textsize.width );
+  int y = rand() % ( screen_h - textsize.height );
+  
+  Mat waitscreen ( screen_h, screen_w, CV_8UC3, Scalar(64,64,64) );
+  putText( waitscreen, text,
+	   Point(x,y), FONT_HERSHEY_SIMPLEX, 1,
+	   Scalar(255,255,255), 1, LINE_AA );
+
+  return waitscreen;
 }
